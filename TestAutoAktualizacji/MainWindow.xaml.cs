0ee5c04a01d99af39e4bca52e19298f75fd683a1
kdpanetpl <kamil_dabkowski1@wp.pl>
2021-12-05 17:04:37 +0100

    initial commit

---
+++ TestAutoAktualizacji/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAutoAktualizacji
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string version;
+        public MainWindow()
+        {
+            version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            InitializeComponent();
+            System.Reflection.Assembly executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var fieVersionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
+            var version2 = fieVersionInfo.FileVersion;
+            label.Content = "0.2.1.13";
+            label2.Content = version;
+            label3.Content = version2;
+        }
+    }
+}
